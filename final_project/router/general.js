37f47cf3b30ef4d9960e73a14a83eb238fc819e3
ciaracianna <96592704+ciaracianna@users.noreply.github.com>
2022-12-29 21:21:15 -0500

    Update general.js
    task 2

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,7 +19,9 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  console.log(books[isbn])
+  return res.send(JSON.stringify(books[isbn]));
  });
   
 // Get book details based on author
